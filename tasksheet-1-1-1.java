a5e275db2f244ee54105e31e5aaefa3658748db1
cppnecr <164193056+cppnecr@users.noreply.github.com>
2024-03-26 15:11:48 +0000

    Task1 Done

---
+++ tasksheet-1-1-1.java
@@ -0,0 +1,9 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
