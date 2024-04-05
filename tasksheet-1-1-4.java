f7d952610b753adb022363ddb8521010cafd20e4
cppnecr <164193056+cppnecr@users.noreply.github.com>
2024-04-05 01:56:52 +0000

    Task4 done

---
+++ tasksheet-1-1-4.java
@@ -0,0 +1,17 @@
+class Grade{
+    
+    public static void main(String[] args) {
+    int score = 87;
+    if (score>=90) {
+        System.out.println("A grade");
+    } else if (score>=80) {
+        System.out.println("B grade");
+    } else if(score>=70){
+        System.out.println("C grade");
+    } else if (score>=60) {
+        System.out.println("D grade");
+    } else if (score>=0) {
+        System.out.println("F grade");
+    }
+    }
+}
