d916ad343dbfd80b7b4e40eb4cd413b410dcfeb6
cppnecr <164193056+cppnecr@users.noreply.github.com>
2024-04-30 10:05:03 +0000

    Task 11 Done

--- tasksheet-1-1-11.java
+++ tasksheet-1-1-11.java
@@ -1,4 +1,32 @@
-
 class Task11 {
-    
+    public static void main(String[] args) {
+        Car c1 = new Car("Toyota", "Fortuner", 2024, 4);
+        Car c2 = new Car("Ford", "Mustang", 2022, 2);
+        c1.displayDetails();
+        c2.displayDetails();
+    }
+}
+
+class Vehicle {
+    String make;
+    String model;
+    int year;
+    Vehicle(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+class Car extends Vehicle {
+    int numberOfDoors;
+    Car(String make, String model, int year, int numberOfDoors){
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    void displayDetails(){
+        System.out.println("Car Details:\nMake: "+this.make+"\nModel: "+this.model+
+        "\nYear: "+this.year+"\nNumber of Doors: "+this.numberOfDoors+"\n");
+    }
 }
